53f6d1a0307a629e682187f48eb770916ed7cd9b
Devon Matthew <dmatthew2024@aol.com>
2024-10-16 15:15:33 -0400

    Update project structure for Vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-    outDir: 'dist', // Explicitly set the output directory
-  },
   server: {
-    port: 5173,
     proxy: {
-      '/.netlify/functions': {
+      '/.netlify': {
         target: 'http://localhost:8888',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/.netlify\/functions/, ''),
       },
     },
   },
-});
+})
