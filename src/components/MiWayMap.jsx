414bb570ed4bc5fa9deb2bbe5e208a24b4681589
Devon Matthew <dmatthew2024@aol.com>
2024-10-18 06:05:35 -0400

    Update Leaflet CSS and fix map rendering issues

--- src/components/MiWayMap.jsx
+++ src/components/MiWayMap.jsx
@@ -7,9 +7,9 @@ import axios from 'axios';
 // Fix for default marker icon
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
 const MiWayMap = ({ searchTerm }) => {
